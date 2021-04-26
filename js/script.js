e2cc72795795f3647e136d904d4b5e9bf2104b70
Mariam Aptsiauri <72245573+mariamapciaurii@users.noreply.github.com>
2021-04-26 16:56:43 +0400

    adding styling

--- js/script.js
+++ js/script.js
@@ -46,12 +46,12 @@ let filteredQuestions = [];
 
 // if startQuiz button clicked
 startButton.onclick = () => {
-  rulesContainer.classList.add("activeTab"); //show info
+  rulesContainer.classList.add("activeTab"); 
 };
 
 // if exitQuiz button clicked
 exitButton.onclick = () => {
-  rulesContainer.classList.remove("activeTab"); //hide info
+  rulesContainer.classList.remove("activeTab"); 
 };
 
 // if continueQuiz button clicked
@@ -80,8 +80,8 @@ function choseDifficulty(difficultyLevel) {
 
 // if restartQuiz button clicked
 restartQuiz.onclick = () => {
-  categoryContainer.classList.add("activeTab"); //show quiz
-  resultsContainer.classList.remove("activeTab"); //hide result
+  categoryContainer.classList.add("activeTab"); 
+  resultsContainer.classList.remove("activeTab");
   questionCount = 0;
   questionNumber = 1;
   userScore = 0;
@@ -96,12 +96,11 @@ quitQuiz.onclick = () => {
   window.location.reload(); //reload the current window
 };
 
-// if Next Que button clicked
+// if Next Question button clicked
 nextButton.onclick = () => {
   if (questionCount < filteredQuestions.length - 1) {
-    //if question count is less than total question length
-    questionCount++; //increment the questionCount value
-    questionNumber++; //increment the questionNumber value
+    questionCount++; 
+    questionNumber++;
     showQuetions(questionCount);
     queCounter(questionNumber);
     clearInterval(counter);
@@ -115,20 +114,19 @@ nextButton.onclick = () => {
   }
 };
 
-// getting questions and options from array
 function showQuetions(index) {
   filteredQuestions = questions[activeCategory].filter(
     (question) => question.difficulty === chosenDifficultyLevel
   );
 
-  //creating a new span and div tag for question and option and passing the value using array index
-  let que_tag =
+  //creating a new span and div tag for question and option
+  let questionTitle =
     "<span>" +
     (index + 1) +
     ". " +
     filteredQuestions[index].question +
     "</span>";
-  let option_tag =
+  let questionAnswers =
     '<div class="option"><span>' +
     filteredQuestions[index].options[0] +
     "</span></div>" +
@@ -142,67 +140,61 @@ function showQuetions(index) {
     filteredQuestions[index].options[3] +
     "</span></div>";
 
-  questionText.innerHTML = que_tag; //adding new span tag inside que_tag
-  optionsList.innerHTML = option_tag; //adding new div tag inside option_tag
+  questionText.innerHTML = questionTitle; 
+  optionsList.innerHTML = questionAnswers;
 
   const option = optionsList.querySelectorAll(".option");
 
-  // set onclick attribute to all available options
   for (i = 0; i < option.length; i++) {
     option[i].setAttribute("onclick", "optionSelected(this)");
   }
 }
-// creating the new div tags which for icons
 let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
 let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 //if user clicked on option
 function optionSelected(answer) {
-  clearInterval(counter); //clear counter
-  clearInterval(counterLine); //clear counterLine
-  let userAns = answer.textContent; //getting user selected option
-  let correcAns = filteredQuestions[questionCount].answer; //getting correct answer from array
-  const allOptions = optionsList.children.length; //getting all option items
+  clearInterval(counter); 
+  clearInterval(counterLine); 
+  let userAns = answer.textContent; 
+  let correcAns = filteredQuestions[questionCount].answer; 
+  const allOptions = optionsList.children.length; 
 
   if (userAns == correcAns) {
-    //if user selected option is equal to array's correct answer
-    userScore += 1; //upgrading score value with 1
-    answer.classList.add("correct"); //adding green color to correct selected option
-    answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
+    userScore += 1; 
+    answer.classList.add("correct"); 
+    answer.insertAdjacentHTML("beforeend", tickIconTag);
   } else {
-    answer.classList.add("incorrect"); //adding red color to correct selected option
-    answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
+    answer.classList.add("incorrect"); 
+    answer.insertAdjacentHTML("beforeend", crossIconTag); 
 
     for (i = 0; i < allOptions; i++) {
       if (optionsList.children[i].textContent == correcAns) {
-        //if there is an option which is matched to an array answer
-        optionsList.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-        optionsList.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+        optionsList.children[i].setAttribute("class", "option correct"); 
+        optionsList.children[i].insertAdjacentHTML("beforeend", tickIconTag);
       }
     }
   }
   for (i = 0; i < allOptions; i++) {
-    optionsList.children[i].classList.add("disabled"); //once user select an option then disabled all options
+    optionsList.children[i].classList.add("disabled"); 
   }
-  nextButton.classList.add("show"); //show the next button if user selected any option
+  nextButton.classList.add("show");
 }
 
 function showResult() {
-  rulesContainer.classList.remove("activeTab"); //hide info
-  quizContainer.classList.remove("activeTab"); //hide quiz
-  resultsContainer.classList.add("activeTab"); //show result
+  rulesContainer.classList.remove("activeTab"); 
+  quizContainer.classList.remove("activeTab"); 
+  resultsContainer.classList.add("activeTab"); 
+
   if (userScore > 3) {
-    // if user scored more than 3
-    //creating a new span tag and passing the user score number and total question number
     let scoreTag =
       "<span>and congrats! 🎉, You got <p>" +
       userScore +
       "</p> out of <p>" +
       filteredQuestions.length +
       "</p></span>";
-    scoreText.innerHTML = scoreTag; //adding new span tag inside scoreText
+    scoreText.innerHTML = scoreTag; 
   } else if (userScore > 1) {
-    // if user scored more than 1
     let scoreTag =
       "<span>and nice 😎, You got <p>" +
       userScore +
@@ -211,7 +203,6 @@ function showResult() {
       "</p></span>";
     scoreText.innerHTML = scoreTag;
   } else {
-    // if user scored less than 1
     let scoreTag =
       "<span>and sorry 😐, You got only <p>" +
       userScore +
@@ -225,37 +216,33 @@ function showResult() {
 function startTimerLine(time) {
   counterLine = setInterval(timer, 29);
   function timer() {
-    time += 1; //upgrading time value with 1
-    timeLine.style.width = time + "px"; //increasing width of timeLine with px by time value
+    time += 1; 
+    timeLine.style.width = time + "px"; 
     if (time > 549) {
-      //if time value is greater than 549
-      clearInterval(counterLine); //clear counterLine
+      clearInterval(counterLine); 
 
-      const allOptions = optionsList.children.length; //getting all option items
-      let correcAns = filteredQuestions[questionCount].answer; //getting correct answer from array
+      const allOptions = optionsList.children.length;
+      let correcAns = filteredQuestions[questionCount].answer; 
       for (i = 0; i < allOptions; i++) {
         if (optionsList.children[i].textContent == correcAns) {
-          //if there is an option which is matched to an array answer
-          optionsList.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-          optionsList.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
-          console.log("Time Off: Auto selected correct answer.");
+          optionsList.children[i].setAttribute("class", "option correct"); 
+          optionsList.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
         }
       }
       for (i = 0; i < allOptions; i++) {
-        optionsList.children[i].classList.add("disabled"); //once user select an option then disabled all options
+        optionsList.children[i].classList.add("disabled"); 
       }
-      nextButton.classList.add("show"); //show the next button if user selected any option
+      nextButton.classList.add("show");
     }
   }
 }
 
 function queCounter(index) {
-  //creating a new span tag and passing the question number and total question
   let totalQueCounTag =
     "<span><p>" +
     index +
     "</p> of <p>" +
     filteredQuestions.length +
     "</p> Questions</span>";
-  bottomQuestionCounter.innerHTML = totalQueCounTag; //adding new span tag inside bottomQuestionCounter
+  bottomQuestionCounter.innerHTML = totalQueCounTag;
 }
